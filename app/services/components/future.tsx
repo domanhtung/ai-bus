568f0dddd6274baa7d210e97e30ab21329ce987f
domanhtung <51911941+domanhtung@users.noreply.github.com>
2024-08-07 15:09:53 +0700

    update: typing speed

--- app/services/components/future.tsx
+++ app/services/components/future.tsx
@@ -6,52 +6,29 @@ import { useEffect, useRef } from "react";
 
 const FutureService = () => {
   const ref = useRef<any>();
-  const defaultText = "The Future is AI, and It Starts Here.";
   let currentIndex = 0;
   let currentText = "";
   let currentQuotesIdx = 0;
-  let direction = 1;
 
   useEffect(() => {
     if (ref?.current) {
       const intervalId = setInterval(() => {
-        if (
-          currentIndex < quotesService?.[currentQuotesIdx]?.length &&
-          direction > 0
-        ) {
+        if (currentIndex < quotesService?.[currentQuotesIdx]?.length) {
           currentText =
             currentText + quotesService?.[currentQuotesIdx]?.[currentIndex];
           ref.current.innerHTML =
             currentText + `<span style="color: #FF6C5C;">|</span>`;
           currentIndex++;
-        } else if (
-          currentIndex >= quotesService?.[currentQuotesIdx]?.length &&
-          direction > 0
-        ) {
-          direction = -1;
-        } else if (
-          currentIndex <= quotesService?.[currentQuotesIdx]?.length &&
-          currentIndex > 0 &&
-          direction < 0
-        ) {
-          currentIndex = currentIndex - 4 <= 0 ? 0 : currentIndex - 3;
-          currentText = quotesService?.[currentQuotesIdx]?.substring(
-            0,
-            currentIndex
-          );
-          ref.current.innerHTML =
-            currentText + `<span style="color: #FF6C5C;">|</span>`;
-        } else if (currentIndex <= 0 && direction < 0) {
+        } else {
           currentIndex = 0;
           currentText = "";
-          direction = 1;
           if (currentQuotesIdx === quotesService?.length - 1) {
             currentQuotesIdx = 0;
           } else {
             currentQuotesIdx++;
           }
         }
-      }, 150);
+      }, 100);
       return () => clearInterval(intervalId);
     }
   }, []);
