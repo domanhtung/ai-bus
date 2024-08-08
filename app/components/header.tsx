66f96651ca35489a8fd57dec8d55dc7381b8fd9a
domanhtung <dotung.mto@gmail.com>
2024-08-08 21:20:24 +0700

    update suspend header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,14 @@ import Link from "next/link";
 import { navbarList, navbarUrl } from "../constants/home";
 import clsx from "clsx";
 import { usePathname, useSearchParams } from "next/navigation";
-import { Dispatch, SetStateAction, useContext, useMemo, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  Suspense,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
 import { ButtonPrimary } from "./custom/button";
 import { ContactContext } from "../context/contact-context";
 
@@ -149,65 +156,70 @@ const Header = () => {
   const [showNavbar, setShowNavbar] = useState<boolean>(false);
 
   return (
-    <div className="h-[76px] overflow-hidden">
-      <div className="fixed top-0 left-0 w-full h-[76px] bg-background border-opacity-20 z-50">
-        <div className="flex h-full max-w-[1512px] mx-auto px-5 xl:px-8 items-center justify-between">
-          <Link href={"/"}>
-            <Image
-              src={"/images/logo.svg"}
-              className="w-[120px] h-[40px] lg:w-[132px] lg:h-[44px]"
-              width={132}
-              height={44}
-              priority
-              alt="logo"
-            />
-          </Link>
-          <div className="hidden lg:flex gap-6 items-center">
-            <div className="flex items-center">
-              {navbarList?.map((navbar) => {
-                return (
-                  <NavName
-                    key={navbar?.title}
-                    pathname={pathname}
-                    navbar={navbar}
-                    setShowNavbar={setShowNavbar}
-                  />
-                );
-              })}
+    <Suspense>
+      <div className="h-[76px] overflow-hidden">
+        <div className="fixed top-0 left-0 w-full h-[76px] bg-background border-opacity-20 z-50">
+          <div className="flex h-full max-w-[1512px] mx-auto px-5 xl:px-8 items-center justify-between">
+            <Link href={"/"}>
+              <Image
+                src={"/images/logo.svg"}
+                className="w-[120px] h-[40px] lg:w-[132px] lg:h-[44px]"
+                width={132}
+                height={44}
+                priority
+                alt="logo"
+              />
+            </Link>
+            <div className="hidden lg:flex gap-6 items-center">
+              <div className="flex items-center">
+                {navbarList?.map((navbar) => {
+                  return (
+                    <NavName
+                      key={navbar?.title}
+                      pathname={pathname}
+                      navbar={navbar}
+                      setShowNavbar={setShowNavbar}
+                    />
+                  );
+                })}
+              </div>
+              <ButtonPrimary
+                title="GET IN TOUCH"
+                onClick={() => showContact()}
+              />
             </div>
+            <div
+              className="grid lg:hidden gap-[7px] p-2 cursor-pointer"
+              onClick={() => setShowNavbar(!showNavbar)}
+            >
+              <div className="w-[24px] h-[3px] bg-primary" />
+              <div className="w-[24px] h-[3px] bg-primary" />
+              <div className="w-[24px] h-[3px] bg-primary" />
+            </div>
+          </div>
+        </div>
+        <div
+          className={clsx(
+            "fixed block lg:hidden w-full h-full top-0 left-0 bg-background z-40 duration-200",
+            showNavbar ? "left-0" : "left-full"
+          )}
+        >
+          <div className="grid pt-20 gap-1 px-5">
+            {navbarList?.map((navbar) => {
+              return (
+                <NavName
+                  key={navbar?.title}
+                  pathname={pathname}
+                  navbar={navbar}
+                  setShowNavbar={setShowNavbar}
+                />
+              );
+            })}
             <ButtonPrimary title="GET IN TOUCH" onClick={() => showContact()} />
           </div>
-          <div
-            className="grid lg:hidden gap-[7px] p-2 cursor-pointer"
-            onClick={() => setShowNavbar(!showNavbar)}
-          >
-            <div className="w-[24px] h-[3px] bg-primary" />
-            <div className="w-[24px] h-[3px] bg-primary" />
-            <div className="w-[24px] h-[3px] bg-primary" />
-          </div>
         </div>
       </div>
-      <div
-        className={clsx(
-          "fixed block lg:hidden w-full h-full top-0 left-0 bg-background z-40 duration-200",
-          showNavbar ? "left-0" : "left-full"
-        )}
-      >
-        <div className="grid pt-20 gap-1 px-5">
-          {navbarList?.map((navbar) => {
-            return (
-              <NavName
-                key={navbar?.title}
-                pathname={pathname}
-                navbar={navbar}
-                setShowNavbar={setShowNavbar}
-              />
-            );
-          })}
-          <ButtonPrimary title="GET IN TOUCH" onClick={() => showContact()} />
-        </div>
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
